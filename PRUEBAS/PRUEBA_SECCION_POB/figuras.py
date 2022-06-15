10c27eb528e69a29af912165d18073e90e442256
SYSTEM-OTMX <gtainside222@gmail.com>
2022-06-14 19:54:04 -0500

    pruebe pendiente

---
+++ PRUEBAS/PRUEBA_SECCION_POB/figuras.py
@@ -0,0 +1,19 @@
+class Figura:
+    def __init__(self,nombre) :
+        self.nombre = nombre
+
+    def area(self):
+        pass
+    def perimetro(self):
+        pass
+
+
+class Rectangulo(Figura):
+    def __init__(self,nombre,base,altura) :
+        super().__init__(nombre)
+
+    
+    pass
+
+class Ciruclo:
+    pass
